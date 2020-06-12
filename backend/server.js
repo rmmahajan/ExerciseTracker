ffc1a318249adbf09e39cf05344d3d62b2f381f1
rmmahajan <manvi.mahajan20@gmail.com>
2020-06-12 17:58:33 +0530

    v3

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 //Database Connection
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true});
+mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
 const connection = mongoose.connection;
 connection.once('open', () => {
     console.log("Connection established.");
