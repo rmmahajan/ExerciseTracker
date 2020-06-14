0ef313f0dc2554512bac3a33bf56657be5bd54f2
rmmahajan <manvi.mahajan20@gmail.com>
2020-06-14 23:37:12 +0530

    Final

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,30 @@
 const express = require('express');
 const cors = require('cors');
-const mongoose = require('mongoose');
-const exercisesRouter = require('./routes/exercises');
-const usersRouter = require('./routes/users');
 require('dotenv').config();
+const mongoose = require('mongoose');
+
 
 const app = express();
-
-
-//Middlewares
+const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
-app.use('/exercises',exercisesRouter);
-app.use('/users',usersRouter);
 
 
+const URI = process.env.ATLAS_URI;
 
-//Database Connection
-const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+mongoose.connect(URI, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
 const connection = mongoose.connection;
 connection.once('open', () => {
-    console.log("Connection established.");
-});
+    console.log("connected to database");
+})
 
-const port = process.env.PORT || 5000;
+const exercisesRoute = require('./routes/exercises');
+const usersRoute = require('./routes/users');
+
+app.use('/exercises', exercisesRoute);
+app.use('/users', usersRoute);
+
 app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
+
+    console.log(`server is running on port ${port}`);
 });
