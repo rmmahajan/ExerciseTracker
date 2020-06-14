afed6d1538c1ab4c440ae6aa99bfbc754b8618ae
rmmahajan <manvi.mahajan20@gmail.com>
2020-06-14 23:47:40 +0530

    Final

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,29 @@
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config();
 const mongoose = require('mongoose');
 
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+
 app.use(cors());
 app.use(express.json());
 
-
-const URI = process.env.ATLAS_URI;
-
-mongoose.connect(URI, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
+);
 const connection = mongoose.connection;
 connection.once('open', () => {
-    console.log("connected to database");
+  console.log("MongoDB database connection established successfully");
 })
 
-const exercisesRoute = require('./routes/exercises');
-const usersRoute = require('./routes/users');
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
 
-app.use('/exercises', exercisesRoute);
-app.use('/users', usersRoute);
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
 
 app.listen(port, () => {
-
-    console.log(`server is running on port ${port}`);
+    console.log(`Server is running on port: ${port}`);
 });
--- src/components/create-user.component.js
+++ src/components/create-user.component.js
@@ -2,56 +2,59 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 export default class CreateUser extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            username: '',
-        }
+  constructor(props) {
+    super(props);
+
+    this.onChangeUsername = this.onChangeUsername.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
+    this.state = {
+      username: ''
+    }
+  }
+
+  onChangeUsername(e) {
+    this.setState({
+      username: e.target.value
+    })
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const user = {
+      username: this.state.username
     }
 
+    console.log(user);
 
-    onChangeUsername = (e) => {
-        this.setState({
-            username: e.target.value
-        });
-    }
+    axios.post('http://localhost:5000/users/add', user)
+      .then(res => console.log(res.data));
 
+    this.setState({
+      username: ''
+    })
+  }
 
-    onSubmit = (e) => {
-        e.preventDefault();
-
-        const user = {
-            username: this.state.username,
-        }
-        console.log(user);
-        axios.post('http://localhost:5000/users/add', user)
-            .then(res => console.log(res.data));
-
-        // window.location = '/';
-        this.setState({
-            username: '',
-        });
-    }
-
-    render() {
-        return (
-            <div>
-                <h3>Create New User</h3>
-                <form onSubmit={(e) => this.onSubmit(e)}>
-                    <div className="form-group">
-                        <label>Username: </label>
-                        <input type="text"
-                            required
-                            className="form-control"
-                            value={this.state.username}
-                            onChange={(e) => this.onChangeUsername(e)}
-                        />
-                    </div>
-                    <div className="form-group">
-                        <input type="submit" value="Create User" className="btn btn-primary" />
-                    </div>
-                </form>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <h3>Create New User</h3>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group"> 
+            <label>Username: </label>
+            <input  type="text"
+                required
+                className="form-control"
+                value={this.state.username}
+                onChange={this.onChangeUsername}
+                />
+          </div>
+          <div className="form-group">
+            <input type="submit" value="Create User" className="btn btn-primary" />
+          </div>
+        </form>
+      </div>
+    )
+  }
 }
